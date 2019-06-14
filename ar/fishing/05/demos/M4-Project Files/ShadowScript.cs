61e2e6425259ba8df8f9350ad74320cd9a98f803
rlopezmachad <rlopezmachad@dxc.com>
2019-06-14 15:46:15 +0930

    Add FIsh AR demo

---
+++ ar/fishing/05/demos/M4-Project Files/ShadowScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowScript : MonoBehaviour {
+	//Makes window for object to follow and renames it to myParent
+	public GameObject myParent;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// set the x and y to match myParent and set Y to 0
+		transform.position = new Vector3 (myParent.transform.position.x, 0, myParent.transform.position.z);	
+	}
+}
---
+++ ar/fishing/06/demos/M5 Project Files/UserDefined_ZAP/AudioDelay2Sec.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioDelay2Sec : MonoBehaviour {
+	public  GameObject PlayerCamera;
+
+
+	// Use this for initialization
+	void Start ()
+
+		{
+		AudioListener.volume = 0f;
+		StartCoroutine ("waitSeconds");
+		}
+
+		IEnumerator waitSeconds()
+		{
+		yield return new WaitForSeconds (2);
+		print ("2 Seconds");
+		AudioListener.volume = .5f;
+		}
+		
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
